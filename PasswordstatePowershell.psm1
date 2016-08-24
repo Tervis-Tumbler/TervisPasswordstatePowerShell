dc97f83459e11a0b38824cad5280f3ec33770017
mastermohl <mastermohl@gmail.com>
2016-08-24 10:03:55 -0400

    Initial Commit

---
+++ PasswordstatePowershell.psm1
@@ -0,0 +1,22 @@
+﻿#Requires -Modules SecureStringFile
+function Get-PasswordStateCredentialFromFile {
+    <#
+    .SYNOPSIS
+     Uses Passwordstate APIKey stored securely using New-SecuredStringToFile and Get-SecuredStringFromFile to output stored credentials from Passwordstate.
+    .DESCRIPTION
+     
+    .EXAMPLE
+    Get-PasswordstateCredentialFromFile
+    .PARAMETER InputFile
+    Fully qualified path to input file
+    #>
+    
+    param(
+        [Parameter(Mandatory)]
+        [string]$SecuredAPIkeyFilePath
+    )
+    $APIKeyURI = Get-SecuredStringFromFile -InputFile $SecuredAPIkeyFilePath
+    $PasswordstateCredentialObject = Invoke-RestMethod $APIKeyURI
+    
+    $PasswordstateCredentialObject
+}
