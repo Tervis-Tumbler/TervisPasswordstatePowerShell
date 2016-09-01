09f5161caaaae5bd61ce27cd65bbd32f7cc6a91a
mastermohl <mastermohl@gmail.com>
2016-09-01 15:01:04 -0400

    Added New-PasswordstateCredentialToFile
    Uses New-Securestringfile to securely store Passwordstate API URL for a
    credential.

--- PasswordstatePowershell.psm1
+++ PasswordstatePowershell.psm1
@@ -20,3 +20,32 @@ function Get-PasswordStateCredentialFromFile {
     
     $PasswordstateCredentialObject
 }
+
+#Requires -Modules SecureStringFile
+function New-PasswordStateCredentialToFile {
+    <#
+    .SYNOPSIS
+     Secure stores Passwordstate APIKey and password using New-SecureStringFile and Get-SecureStringFile to be used in scripts.
+    .DESCRIPTION
+     
+    .EXAMPLE
+    Set-PasswordstateCredentialToFile
+    .PARAMETER APIKey
+    APIKey for Password list
+    .PARAMETER PasswordID
+    PasswordID for credential within Passwordstate
+    #>
+    
+    param(
+        [Parameter(Mandatory)]
+        [string]$DestinationSecureFile,
+        [Parameter(Mandatory)]
+        [string]$APIKey,
+        [Parameter(Mandatory)]
+        [string]$PasswordID
+    )
+    
+    $URLToPasswordstateCredential = "https://passwordstate/api/passwords/$PasswordID`?apikey=$APIKEY"
+    $SecureString = ConvertTo-SecureString -String $URLToPasswordstateCredential -AsPlainText -Force
+    New-SecureStringFile -OutputFile $DestinationSecureFile -SecureString $SecureString
+}
