7f8a8b56ba4c33906f50e9d171dce12b2d50a2e6
Greg <gkjono@tervis.com>
2017-04-27 10:17:23 -0400

    Added Get-PasswordstateDocument function

--- PasswordstatePowershell.psm1
+++ PasswordstatePowershell.psm1
@@ -140,3 +140,12 @@ function New-PasswordstateEntry {
 
 
 }
+
+Function Get-PasswordstateDocument {
+    param (
+        [Parameter(Mandatory)][string]$DocumentID,
+        [string]$PasswordstateListAPIKey = $(Get-PasswordStateAPIKey)
+    )
+    $URLToPasswordstateCredential = "https://passwordstate/api/document/password/$DocumentID`?apikey=$PasswordstateListAPIKey"
+    Invoke-RestMethod $URLToPasswordstateCredential
+}
