bfd93459fb8619d1cc5b7bcc402a9df18429b5d8
ChrisMagnuson <hourglasssand@gmail.com>
2018-06-11 14:17:45 -0400

    Fix various bugs

--- TervisPasswordstatePowershell.psm1
+++ TervisPasswordstatePowershell.psm1
@@ -99,20 +99,33 @@ function Install-PasswordstateServicerestartScheduledTask {
     }
 }
 
+function Get-TervisPasswordStateApplicationPasswordTitle {
+    param (
+        [Parameter(Mandatory)][ValidateSet("LocalAdministrator")]$Type,
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$ApplicationName,
+        [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$EnvironmentName       
+    )
+    process {
+        "$ApplicationName Application Node Local Administrator $EnvironmentName"
+    }
+}
+
 function New-TervisPasswordStateApplicationPassword {
     param (
         [Parameter(Mandatory)][ValidateSet("LocalAdministrator")]$Type,
         [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$ApplicationName,
         [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$EnvironmentName
     )
-    if ($Type -eq "LocalAdministrator") {
-        $PasswordTitle = "$ApplicationName Application Node Local Administrator $EnvironmentName"
-        $Password = Find-PasswordstatePassword -Title $PasswordTitle -ErrorAction SilentlyContinue
-        if (-not $Password) {
-            $PasswordList = Find-PasswordstateList -PasswordList "Windows Server Applications Administrator"
-            New-PasswordstatePassword -GeneratePassword $true -PasswordListID $PasswordList.PasswordListID -Title $PasswordTitle -UserName ".\administrator"
-        } else {
-            $Password
+    process {
+        if ($Type -eq "LocalAdministrator") {
+            $PasswordTitle = Get-TervisPasswordStateApplicationPasswordTitle @PSBoundParameters
+            $Password = Find-PasswordstatePassword -Title $PasswordTitle -ErrorAction SilentlyContinue
+            if (-not $Password) {
+                $PasswordList = Find-PasswordstateList -PasswordList "Windows Server Applications Administrator"
+                New-PasswordstatePassword -GeneratePassword $true -PasswordListID $PasswordList.PasswordListID -Title $PasswordTitle -UserName ".\administrator"
+            } else {
+                $Password
+            }
         }
     }
 }
@@ -124,8 +137,10 @@ function Get-TervisPasswordStateApplicationPassword {
         [Parameter(Mandatory,ValueFromPipelineByPropertyName)]$EnvironmentName,
         [Switch]$AsCredential
     )
-    if ($Type -eq "LocalAdministrator") {
-        $PasswordTitle = "$ApplicationName Application Node Local Administrator"
-        $Password = Find-PasswordstatePassword -Title $PasswordTitle -ErrorAction SilentlyContinue -AsCredential:$AsCredential
+    process {
+        if ($Type -eq "LocalAdministrator") {
+            $PasswordTitle = Get-TervisPasswordStateApplicationPasswordTitle @PSBoundParameters
+            Find-PasswordstatePassword -Title $PasswordTitle -ErrorAction SilentlyContinue -AsCredential:$AsCredential
+        }    
     }
 }
